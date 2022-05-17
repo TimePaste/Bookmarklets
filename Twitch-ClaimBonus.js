b04fd46c6a9132d98c9a365144a78dd470a356ba
TimePaste <49597696+TimePaste@users.noreply.github.com>
2022-05-17 16:23:33 -0500

    Create Twitch-ClaimBonus.js

---
+++ Twitch-ClaimBonus.js
@@ -0,0 +1,3 @@
+//Twitch -- claim a bonus bookmark
+
+javascript:function myMethod( ) { var q = document.querySelector(".claimable-bonus__icon"); if (q != null) q.click(); var t = document.querySelector(".claimable-bonus__icon"); if (t != null) t.click();} var timer = setInterval(myMethod, 1000);
